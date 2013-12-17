3369ca8862c8b7fd99e37bc74155488ea9a346cd
Manas Alekar <manas@ooyala.com>
2013-12-17 14:06:31 -0800

    Switch logging to syslog for performance.

--- example/router.go
+++ example/router.go
@@ -3,11 +3,22 @@ package main
 import (
 	"atlantis/router/lb"
 	"flag"
+	"log"
+	"log/syslog"
 )
 
 var servers string
 
 func main() {
+	// Logging to syslog is more performant, which matters.
+	w, err := syslog.New(syslog.LOG_INFO, "atlantis-router")
+	if err != nil {
+		log.Println("[ERROR] cannot log to syslog!")
+	} else {
+		log.SetOutput(w)
+		log.SetFlags(0)
+	}
+
 	flag.StringVar(&servers, "zk", "localhost:2181", "zookeeper connection string")
 	balancer := lb.New(servers)
 	balancer.Run()
